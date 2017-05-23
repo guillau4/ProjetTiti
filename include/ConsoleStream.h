75e0c0b58acb6b1abfaef4135b78da35453a5cf5
Watashiku <adelbeque@gmail.com>
2017-05-23 12:39:02 +0200

    azeffverjh vuae

---
+++ include/ConsoleStream.h
@@ -0,0 +1,19 @@
+#ifndef CONSOLESTREAM_H
+#define CONSOLESTREAM_H
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+class ConsoleStream
+{
+    public:
+        ConsoleStream();
+        virtual ~ConsoleStream();
+
+    protected:
+
+    private:
+};
+
+#endif // CONSOLESTREAM_H
